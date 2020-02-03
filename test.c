6d19781ecb0c594908b2cbb224f13f3095eeef58
evgenkarlson <V1z9l7o3m5@mail.ru>
2020-02-03 23:42:47 +0300

    Ecole42

--- test.c
+++ test.c
@@ -1,16 +1,27 @@
-#include <unistd.h>
 
+
+#include <unistd.h>
 
 int		main(int argc, char *argv[])
 {
 	int		i;
+
 	i = 0;
-	if (argc > 1)
+	if (argc != 2)
+		write(1, "z\n", 2);
+	else
 	{
-		while(argv[argc - 1][i])
-			write(1, &argv[argc - 1][i++], 1);
+		while (argv[1][i])
+		{
+			if (argv[1][i] == 'z')
+			{
+				write(1, "z", 1);
+				break ;
+			}
+			i += 1;
+		}
+		write(1, "\n", 1);
 	}
-	write(1, "\n", 1);
 	return (0);
 }
 
