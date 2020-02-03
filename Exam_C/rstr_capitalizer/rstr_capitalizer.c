77d4a007df2bf1bd24a21fb01ae7cd2013ad49fe
evgenkarlson <V1z9l7o3m5@mail.ru>
2020-02-03 17:53:44 +0300

    Ecole42

--- Exam_C/rstr_capitalizer/rstr_capitalizer.c
+++ Exam_C/rstr_capitalizer/rstr_capitalizer.c
@@ -19,9 +19,9 @@ void	rstr_capitalizer(char *str)
 	i = 0;
 	while (str[i])
 	{
-		if (str[i] >= 'A' && 'Z' >= str[i])
+		if (str[i] >= 'A' && str[i] <= 'Z')
 			str[i] += 32;
-		if ((str[i] >= 'a' && 'z' >= str[i]) && (str[i + 1] == ' ' || \
+		if ((str[i] >= 'a' && str[i] <= 'z') && (str[i + 1] == ' ' || \
 			str[i + 1] == '\t' || str[i + 1] == '\0'))
 			str[i] -= 32;
 		write(1, &str[i++], 1);
