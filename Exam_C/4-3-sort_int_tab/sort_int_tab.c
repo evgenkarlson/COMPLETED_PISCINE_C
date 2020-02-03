0b854eff6e9270c195691ba9127a9fc5d92cc734
evgenkarlson <V1z9l7o3m5@mail.ru>
2020-02-03 16:15:44 +0300

    Ecole42

--- Exam_C/4-3-sort_int_tab/sort_int_tab.c
+++ Exam_C/4-3-sort_int_tab/sort_int_tab.c
@@ -10,32 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
-void	sort_int_tab(int *tab, unsigned int size)
-{
-	int				temp;
-	unsigned int 	i;
-	unsigned int 	j;
-
-	i = 0;
-	while (i < size - 1)
-	{
-		j = i;
-		while (j < size)
-		{
-			if (tab[i] > tab[j])
-			{
-				temp = tab[i];
-				tab[i] = tab[j];
-				tab[j] = temp;
-			}
-			j += 1;
-		}
-		i += 1;
-	}
-}
-
-void	sort_int_tab(int *tab, unsigned int size)
+void	sort_int_tab(int *tab, int size)
 {
 	int i;
 	int j;
@@ -59,3 +34,40 @@ void	sort_int_tab(int *tab, unsigned int size)
 		i++;
 	}
 }
+
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+
+void	sort_int_tab(int *tab, int size)
+{
+	int				temp;
+	unsigned int 	i;
+	unsigned int 	j;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		j = i;
+		while (j < size)
+		{
+			if (tab[i] > tab[j])
+			{
+				temp = tab[i];
+				tab[i] = tab[j];
+				tab[j] = temp;
+			}
+			j += 1;
+		}
+		i += 1;
+	}
+}
+
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+
