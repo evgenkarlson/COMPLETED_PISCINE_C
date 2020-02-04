8055149935852debb105d762d61611de76139c6d
evgenkarlson <V1z9l7o3m5@mail.ru>
2020-02-04 11:47:41 +0300

    Ecole42

--- Exam_C/1-0-repeat_alpha/repeat_alpha.c
+++ Exam_C/1-0-repeat_alpha/repeat_alpha.c
@@ -10,6 +10,57 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+/* **************************************************************************
+
+
+Assignment name  : repeat_alpha
+Expected files   : repeat_alpha.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program called repeat_alpha that takes a string and display it
+repeating each alphabetical character as many times as its alphabetical index,
+followed by a newline.
+Напишите программу с именем repeat_alpha, которая берет строку и отображает ее,
+повторяя каждый алфавитный символ столько раз, сколько его алфавитный указатель, 
+за которым следует новая строка.
+
+'a' becomes 'a', 'b' becomes 'bb', 'e' becomes 'eeeee', etc...
+
+Case remains unchanged.
+Дело остается без изменений.
+
+
+If the number of arguments is not 1, just display a newline.
+Если количество аргументов не равно 1, просто отобразите новую строку.
+
+Examples:
+Примеры:
+
+
+$>./repeat_alpha "abc"
+abbccc
+$>./repeat_alpha "Alex." | cat -e
+Alllllllllllleeeeexxxxxxxxxxxxxxxxxxxxxxxx.$
+$>./repeat_alpha 'abacadaba 42!' | cat -e
+abbacccaddddabba 42!$
+$>./repeat_alpha | cat -e
+$
+$>
+$>./repeat_alpha "" | cat -e
+$
+$>
+
+
+
+   ************************************************************************** */
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+
 #include <unistd.h>
 
 int		main(int argc, char *argv[])
