e5f7a759f1db55f5a173e55c3cfaa3a885742e9d
evgenkarlson <V1z9l7o3m5@mail.ru.com>
2020-02-04 18:12:42 +0300

    Ecole42

--- Exam_C/1-0-rotone/rotone.c
+++ Exam_C/1-0-rotone/rotone.c
@@ -53,6 +53,38 @@ $>
 
 #include <unistd.h>
 
+int		main(int argc, char *argv[])
+{
+	int		i;
+	char	letter;
+	i = 0;
+	if (argc == 2)
+	{
+		while (argv[1][i])
+		{
+			letter = argv[1][i];
+			if (argv[1][i] >= 'A' && argv[1][i] <= 'Y')
+				letter += 1;
+			if (argv[1][i] >= 'a' && argv[1][i] <= 'y')
+				letter += 1;
+			if (argv[1][i] == 'z' || argv[1][i] == 'Z')
+				letter -= 25;
+			write(1, &letter, 1);
+			i += 1;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
+
+
+/* ************************************************************************** */
+/* ********************************_OR_THAT_:)******************************* */
+/* ************************************************************************** */
+
+
+#include <unistd.h>
+
 int		main(int argc, char **argv)
 {
 	int i;
@@ -83,36 +115,4 @@ int		main(int argc, char **argv)
 		}
 		write(1, "\n", 1);
 	}
-}
-
-
-/* ************************************************************************** */
-/* ********************************_OR_THAT_:)******************************* */
-/* ************************************************************************** */
-
-
-#include <unistd.h>
-
-int		main(int argc, char *argv[])
-{
-	int		i;
-	char	letter;
-	i = 0;
-	if (argc == 2)
-	{
-		while (argv[1][i])
-		{
-			letter = argv[1][i];
-			if (argv[1][i] >= 'A' && argv[1][i] <= 'Y')
-				letter += 1;
-			if (argv[1][i] >= 'a' && argv[1][i] <= 'y')
-				letter += 1;
-			if (argv[1][i] == 'z' || argv[1][i] == 'Z')
-				letter -= 25;
-			write(1, &letter, 1);
-			i += 1;
-		}
-	}
-	write(1, "\n", 1);
-	return (0);
 }
