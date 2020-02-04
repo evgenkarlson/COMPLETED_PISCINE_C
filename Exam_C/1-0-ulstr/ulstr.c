195d301a17c76418b021134f479752f8bb298114
evgenkarlson <V1z9l7o3m5@mail.ru.com>
2020-02-04 18:22:20 +0300

    Ecole42

--- Exam_C/1-0-ulstr/ulstr.c
+++ Exam_C/1-0-ulstr/ulstr.c
@@ -10,6 +10,41 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+/* **************************************************************************
+
+
+Assignment name  : ulstr
+Expected files   : ulstr.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes a string and reverses the case of all its letters.
+Other characters remain unchanged.
+
+You must display the result followed by a '\n'.
+
+If the number of arguments is not 1, the program displays '\n'.
+
+Examples :
+
+$>./ulstr "L'eSPrit nE peUt plUs pRogResSer s'Il staGne et sI peRsIsTent VAnIte et auto-justification." | cat -e
+l'EspRIT Ne PEuT PLuS PrOGrESsER S'iL STAgNE ET Si PErSiStENT vaNiTE ET AUTO-JUSTIFICATION.$
+$>./ulstr "S'enTOuRer dE sECreT eSt uN sIGnE De mAnQuE De coNNaiSSanCe.  " | cat -e
+s'ENtoUrER De SecREt EsT Un SigNe dE MaNqUe dE COnnAIssANcE.  $
+$>./ulstr "3:21 Ba  tOut  moUn ki Ka di KE m'en Ka fe fot" | cat -e
+3:21 bA  ToUT  MOuN KI kA DI ke M'EN kA FE FOT$
+$>./ulstr | cat -e
+$
+
+
+
+   ************************************************************************** */
+/* ************************************************************************** */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
 void	ft_putchar(char c)
@@ -50,5 +85,36 @@ int main(int argc, char **argv)
 
 	ft_putstr(str);
 	ft_putchar('\n');
+	return (0);
+}
+
+
+/* ************************************************************************** */
+/* ********************************_OR_THAT_:)******************************* */
+/* ************************************************************************** */
+
+
+#include <unistd.h>
+
+int		main(int argc, char *argv[])
+{
+	int		i;
+	char	letter;
+
+	i = 0;
+	if (argc == 2)
+	{
+		while(argv[1][i])
+		{
+			letter = argv[1][i];
+			if (argv[1][i] >= 'A' && 'Z' >= argv[1][i])
+				letter += 32;
+			if (argv[1][i] >= 'a' && 'z' >= argv[1][i])
+				letter -= 32;
+			write(1, &letter, 1);
+			i += 1;
+		}
+	}
+	write(1, "\n", 1);
 	return (0);
 }
