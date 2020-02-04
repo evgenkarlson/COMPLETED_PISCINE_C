92e742b1b1837e3ff76421cd6389c2ad7f496790
evgenkarlson <V1z9l7o3m5@mail.ru.com>
2020-02-04 18:06:31 +0300

    Ecole42

--- Exam_C/1-0-rotone/rotone.c
+++ Exam_C/1-0-rotone/rotone.c
@@ -10,24 +10,48 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+/* **************************************************************************
+
+
+Assignment name  : rotone
+Expected files   : rotone.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes a string and displays it, replacing each of its
+letters by the next one in alphabetical order.
+
+'z' becomes 'a' and 'Z' becomes 'A'. Case remains unaffected.
+
+The output will be followed by a \n.
+
+If the number of arguments is not 1, the program displays \n.
+
+Example:
+
+$>./rotone "abc"
+bcd
+$>./rotone "Les stagiaires du staff ne sentent pas toujours tres bon." | cat -e
+Mft tubhjbjsft ev tubgg of tfoufou qbt upvkpvst usft cpo.$
+$>./rotone "AkjhZ zLKIJz , 23y " | cat -e
+BlkiA aMLJKa , 23z $
+$>./rotone | cat -e
+$
+$>
+$>./rotone "" | cat -e
+$
+$>
+
+
+   ************************************************************************** */
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+
 #include <unistd.h>
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	ft_putstr(char *str)
-{
-	int i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		ft_putchar(str[i]);
-		i++;
-	}
-}
 
 int		main(int argc, char **argv)
 {
@@ -35,7 +59,7 @@ int		main(int argc, char **argv)
 
 	if (argc != 2)
 	{
-		ft_putchar('\n');
+		write(1, "\n", 1);
 		return (0);
 	}
 	else
@@ -51,7 +75,44 @@ int		main(int argc, char **argv)
 				argv[1][i] -= 25;
 			i++;
 		}
-		ft_putstr(argv[1]);
-		ft_putchar('\n');
+		i = 0;
+		while (argv[1][i] != '\0')
+		{
+			write(1, argv[1][i], 1);
+			i++;
+		}
+		write(1, "\n", 1);
 	}
+}
+
+
+/* ************************************************************************** */
+/* ********************************_OR_THAT_:)******************************* */
+/* ************************************************************************** */
+
+
+#include <unistd.h>
+
+int		main(int argc, char *argv[])
+{
+	int		i;
+	char	letter;
+	i = 0;
+	if (argc == 2)
+	{
+		while (argv[1][i])
+		{
+			letter = argv[1][i];
+			if (argv[1][i] >= 'A' && argv[1][i] <= 'Y')
+				letter += 1;
+			if (argv[1][i] >= 'a' && argv[1][i] <= 'y')
+				letter += 1;
+			if (argv[1][i] == 'z' || argv[1][i] == 'Z')
+				letter -= 25;
+			write(1, &letter, 1);
+			i += 1;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
 }
