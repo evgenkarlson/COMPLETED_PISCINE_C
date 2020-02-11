7f62d7f3cafc4b705682594e43e80d15efb1d30b
evgenkarlson <V1z9l7o3m5@mail.ru>
2020-02-11 20:59:20 +0300

    Ecole42

--- Rush00/v0.0/main.c
+++ Rush00/v0.0/main.c
@@ -15,9 +15,5 @@ void	rush(int x, int y);
 int		main(void)
 {
 	rush(5, 3);
-	rush(5, 1);
-	rush(1, 1);
-	rush(1, 5);
-	rush(4, 4);
 	return (0);
 }
--- test.c
+++ test.c
@@ -62,10 +62,7 @@ void	rush(int x, int y)
 int		main(void)
 {
 	rush(5, 3);
-	rush(5, 1);
-	rush(1, 1);
-	rush(1, 5);
-	rush(4, 4);
+	
 	return (0);
 }
 
