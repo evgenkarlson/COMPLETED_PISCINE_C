7349db17ba848a95c84d0026e741199463e3cd65
evgenkarlson <V1z9l7o3m5@mail.ru>
2020-02-12 23:59:49 +0300

    Ecole42

--- d06/ex01/test.c
+++ d06/ex01/test.c
@@ -33,11 +33,13 @@ int     main(int argc, char **argv)
 	int	i;
 
 	i = 0;
-	argc = 0;
-	while (argv[0][i])
+	if (argc > 0)
 	{
-		ft_putchar(argv[0][i]);
-		i++;
+		while (argv[0][i])
+		{
+			ft_putchar(argv[0][i]);
+			i++;
+		}
 	}
 	return (0);
 }
