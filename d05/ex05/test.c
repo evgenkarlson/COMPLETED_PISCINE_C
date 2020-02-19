0e856585caa1cd55c6de7679aad97da0af2bcb63
evgenkarlson <V1z9l7o3m5@mail.ru>
2020-02-19 18:00:59 +0300

    Ecole42

--- d05/ex05/test.c
+++ d05/ex05/test.c
@@ -76,20 +76,20 @@ char	*ft_strstr(char *str, char *to_find)
 {
 	int	i;
 	int	z;
-	int	to_find_size;
+	int	lenght;
 
 	i = 0;
 	z = 0;
-	to_find_size = 0;
-	while (to_find[to_find_size])
-		to_find_size++;
-	if (to_find_size == 0)
+	lenght = 0;
+	while (to_find[lenght])
+		lenght++;
+	if (lenght == 0)
 		return (str);
 	while (str[i])
 	{
 		while (to_find[z] == str[i + z])
 		{
-			if (z == to_find_size - 1)
+			if (z == lenght - 1)
 				return (str + i);
 			z++;
 		}
