490997feb8020b55ed146f3191cd508c01838dea
evgenkarlson <V1z9l7o3m5@mail.ru>
2020-02-22 17:23:14 +0300

    Ecole42

--- Exam_C/1-0-rotone.c
+++ Exam_C/1-0-rotone.c
@@ -25,6 +25,7 @@ Write a program that takes a string and displays it, replacing each of its
 letters by the next one in alphabetical order.
 'z' becomes 'a' and 'Z' becomes 'A'. Case remains unaffected.
 The output will be followed by a \n.
+
 Напишите программу, которая берет строку и отображает ее, заменяя каждую
 из ее букв на следующую в алфавитном порядке.
 «z» становится «a», а «Z» становится «A». Дело остается без изменений.
