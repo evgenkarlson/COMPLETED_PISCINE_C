4264f548746287329bd6e9a64d9c1a129809a8de
evgenkarlson <V1z9l7o3m5@mail.ru>
2020-03-02 04:25:45 +0300

    Ecole42

--- d07/ex04/ft_split_whitespaces.c
+++ d07/ex04/ft_split_whitespaces.c
@@ -65,6 +65,9 @@ int	main()
 /* ******************************_ИЛИ_*************************************** */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #define IS_SPACE(c) (((c == ' ' || c == '\t' || c == '\n') ? 1 : 0))
 
 int		g_words = 0;
@@ -140,6 +143,9 @@ char	**ft_split_whitespaces(char *str)
 /* ************************************************************************** */
 /* ******************************_ИЛИ_*************************************** */
 /* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
 
 static int	ft_wordcount(char *s, char c)
 {
@@ -285,3 +291,77 @@ char	**ft_split_whitespaces(char *str)
 /* ************************************************************************** */
 /* ************************************************************************** */
 /* ************************************************************************** */
+
+
+#include <stdlib.h>
+#define IS_SPACE(c) (((c == ' ' || c == '\t' || c == '\n') ? 1 : 0))
+
+int		g_words = 0;
+
+int		calc_numberofwords(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[i] == '\0')
+		return (0);
+	while (str[i] != '\0')
+	{
+		if (!(g_words) && (!(IS_SPACE(str[i]))))
+			g_words++;
+		else if (IS_SPACE(str[i - 1]) && !(IS_SPACE(str[i])))
+			g_words++;
+		i++;
+	}
+	if (g_words)
+		return (1);
+	else
+		return (0);
+}
+
+char	*createword(char *str, int i, int j)
+{
+	char	*word;
+	int		o;
+
+	o = 0;
+	word = (char*)malloc(sizeof(char) * (j - i + 1));
+	while (i < j)
+	{
+		word[o] = str[i];
+		i++;
+		o++;
+	}
+	word[o] = '\0';
+	return (word);
+}
+
+int		g_j = 0;
+int		g_i = 0;
+
+char	**ft_split_whitespaces(char *str)
+{
+	char	**arr;
+	int		index;
+
+	index = 0;
+	if (calc_numberofwords(str))
+	{
+		arr = (char**)malloc(sizeof(char*) * (g_words + 1));
+		while (index < g_words)
+		{
+			while (IS_SPACE(str[g_i]) && (str[g_i] != '\0'))
+				g_i++;
+			g_j = g_i;
+			while (!(IS_SPACE(str[g_j])) && (str[g_j] != '\0'))
+				g_j++;
+			arr[index] = createword(str, g_i, g_j);
+			g_i = g_j + 1;
+			index++;
+		}
+	}
+	else
+		arr = (char**)malloc(sizeof(char*));
+	arr[index] = 0;
+	return (arr);
+}
