d87db015c277f77815d79402fe3545c8280766c5
evgenkarlson <V1z9l7o3m5@mail.ru>
2020-03-18 02:00:29 +0300

    Ecole42

--- d05/ex20(ft_putnbr_base)/ft_putnbr_base.c
+++ d05/ex20(ft_putnbr_base)/ft_putnbr_base.c
@@ -251,3 +251,122 @@ void	ft_putnbr_base(int nbr, char *base)
 
 
 
+
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+
+ 
+int     ft_strlen(char *str)
+{
+    int     x;
+ 
+    x = 0;
+    while (str[x] != '\0')
+        x++;
+    return (x);
+}
+ 
+void    ft_putnbr_base(int nbr, char *base)
+{
+    int     *conv_num;
+    int     index;
+ 
+    index = 0;
+    while (nbr != 0)
+    {
+        conv_num[index] = nbr % ft_strlen(base);
+        nbr = nbr / ft_strlen(base);
+        index++;
+    }
+    index--;
+    while (index > 0)
+    {
+        ft_putchar(base[conv_num[index]]);
+        index--;
+    }
+}
+ 
+int     main(void)
+{
+    int     nbr;
+    char    *base;
+ 
+    nbr = 4322;
+    base = "0123456789ABCDE";
+    ft_putnbr_base(nbr, base);
+    return (0);
+}
+
+
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+
+static int	ft_strlen(char *str)
+{
+	int len;
+
+	len = 0;
+	while (*str++ != '\0')
+	{
+		++len;
+	}
+	return (len);
+}
+
+static void	ft_putnbr_base_n(int nbr, char *base, int n)
+{
+	if (nbr < 0)
+	{
+		ft_putchar('-');
+		if (nbr == -2147483648)
+		{
+			ft_putnbr_base_n(2, base, n);
+			ft_putnbr_base_n(147483648, base, n);
+		}
+		else
+			ft_putnbr_base_n(-nbr, base, n);
+	}
+	else if (nbr < n)
+		ft_putchar(base[nbr]);
+	else
+	{
+		ft_putnbr_base_n(nbr / n, base, n);
+		ft_putchar(base[nbr % n]);
+	}
+}
+
+static int	ft_strchar(char *str, char to_find)
+{
+	while (*str != '\0')
+		if (to_find == *str++)
+			return (1);
+	return (0);
+}
+
+static int	ft_check_base(char *base, int n)
+{
+	while (n)
+	{
+		if (base[n] == '+' || base[n] == '-')
+			return (0);
+		if (ft_strchar(base + n + 1, base[n]))
+			return (0);
+		--n;
+	}
+	return (1);
+}
+
+void		ft_putnbr_base(int nbr, char *base)
+{
+	int		base_len;
+
+	if (base && (base_len = ft_strlen(base)) > 1 &&
+		ft_check_base(base, base_len))
+		ft_putnbr_base_n(nbr, base, base_len);
+}
