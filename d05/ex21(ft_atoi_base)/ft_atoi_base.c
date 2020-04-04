1bb210d9b27af82ee36bfd4853137ccec9f26361
evgenkarlson <V1z9l7o3m5@mail.ru>
2020-04-05 02:49:03 +0300

    code

--- d05/ex21(ft_atoi_base)/ft_atoi_base.c
+++ d05/ex21(ft_atoi_base)/ft_atoi_base.c
@@ -63,6 +63,105 @@
 /* ************************************************************************** */
 
 
+
+
+int		ft_ctoi(char c)				
+{
+	if (c >= '0' && c <= '9')
+		return (c - '0');
+	if (c >= 'A' && c <= 'F')
+		return (c - 'A' + 10);
+	if (c >= 'a' && c <= 'f')
+		return (c - 'a' + 10);
+	return (-1);
+}
+
+
+
+int		ft_pow(int nb, int power)
+{
+	int	result;
+
+	result = 1;
+	if(power < 0)
+		return (0);
+	if(power == 0)
+		return (1);
+	while (power--)
+		result *= nb;
+	return (result);
+}
+
+
+
+int		ft_check_base(char *base)
+{
+	int	i;
+	int	z;
+
+	i = 0;
+	z = 0;
+	if (!base || !base[1])
+		return (0);
+	while (base[i])
+	{
+		z = i + 1;
+		if (!((base[i] >= '0' && base[i] <= '9') || (base[i] >= 'a' \
+				&& base[i] <= 'z') || (base[i] >= 'A' && base[i] <= 'Z')))
+			return (0);
+		while (base[z])
+			if (base[i] == base[z++])
+				return (0);
+		i++;
+	}
+	return (i);
+}
+
+
+
+int		ft_atoi_base(char *str, char *base)
+{
+	int		num;
+	int		negative;
+	int		i;
+	int		pow;
+	int		base_type;
+
+	pow = 0;
+	i = 0;
+	negative = 1;
+	num = 0;
+	base_type = ft_check_base(base);
+	if(base_type)
+	{
+		if (*str == '-')
+		{
+			i++;
+			negative = -1;
+		}
+		while (str[i])
+			i++;
+		while (--i >= 0)
+		{
+			if (ft_ctoi(str[i]) != -1 && ft_ctoi(str[i]) < base_type)
+				num += ft_ctoi(str[i]) * ft_pow(base_type, pow++);
+		}
+		return (num * negative);
+	}
+	return (0);
+}
+
+
+
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+
+
+
+
 static int	ft_isspace(int c)
 {
 	return (c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r'
@@ -202,103 +301,6 @@ int		ft_atoi_base(char *str, char *base)
 	v = parse_positive(str, base, basen);
 	v *= -(is_positive + !is_positive);
 	return (v);
-}
-
-
-
-
-/* ************************************************************************** */
-/* ************************************************************************** */
-/* ************************************************************************** */
-
-
-
-
-
-int		ft_ctoi(char c)				
-{
-	if (c >= '0' && c <= '9')
-		return (c - '0');
-	if (c >= 'A' && c <= 'F')
-		return (c - 'A' + 10);
-	if (c >= 'a' && c <= 'f')
-		return (c - 'a' + 10);
-	return (-1);
-}
-
-
-
-int		ft_pow(int nb, int power)
-{
-	int	result;
-
-	result = 1;
-	if(power < 0)
-		return (0);
-	if(power == 0)
-		return (0);
-	while (power--)
-		result *= nb;
-	return (result);
-}
-
-
-
-int		ft_check_base(char *base)
-{
-	int	i;
-	int	z;
-
-	i = 0;
-	z = 0;
-	if (!base || !base[1])
-		return (0);
-	while (base[i])
-	{
-		z = i + 1;
-		if (!((base[i] >= '0' && base[i] <= '9') || (base[i] >= 'a' \
-				&& base[i] <= 'z') || (base[i] >= 'A' && base[i] <= 'Z')))
-			return (0);
-		while (base[z])
-			if (base[i] == base[z++])
-				return (0);
-		i++;
-	}
-	return (i);
-}
-
-
-
-int		ft_atoi_base(char *str, char *base)
-{
-	int		num;
-	int		negative;
-	int		i;
-	int		pow;
-	int		base_type;
-
-	pow = 0;
-	i = 0;
-	negative = 1;
-	num = 0;
-	base_type = ft_check_base(base);
-	if(base_type)
-	{
-		if (*str == '-')
-		{
-			i++;
-			negative = -1;
-		}
-		while (str[i])
-			i++;
-		while (--i >= 0)
-		{
-			if (ft_ctoi(str[i]) != -1 && ft_ctoi(str[i]) < base_type)
-				num += ft_ctoi(str[i]) * ft_pow(base_type, pow++);
-		}
-		return (num * negative);
-	}
-	return (0);
 }
 
 
