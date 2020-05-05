fce93bd983157157fe6058f6b0626c25b15f9e2c
evgenkarlson <V1z9l7o3m5@mail.ru>
2020-05-06 01:25:33 +0300

    6.05.2020

--- d08/ex05(ft_param_to_tab)/ft_param_to_tab.c
+++ d08/ex05(ft_param_to_tab)/ft_param_to_tab.c
@@ -12,6 +12,55 @@
 
 #include "ft_stock_par.h"
 
+static char			*ft_strdup(char *src)
+{
+	int				i;
+	char			*dst;
+
+	i = 0;
+	while (src[i])
+		i += 1;
+	if (!(dst = (char *)malloc(sizeof(char) * (i + 1))))
+		return (NULL);
+	i = -1;
+	while (src[++i])
+		dst[i] = src[i];
+	dst[i] = '\0';
+	return (dst);
+}
+
+struct s_stock_par	*ft_param_to_tab(int ac, char **av)
+{
+	int				i;
+	int				j;
+	t_stock_par		*stk;
+
+	if (!(stk = (t_stock_par*)malloc(sizeof(t_stock_par) * (ac + 1))))
+		return (NULL);
+	i = 0;
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j])
+			j++;
+		stk[i]->size_param = j;
+		stk[i]->str = av[i];
+		stk[i]->copy = ft_strdup(av[i]);
+		stk[i]->tab = ft_split_whitespaces(av[i]);
+		i++;
+	}
+	stk[i]->str = 0;
+	return (stk);
+}
+
+
+/* ************************************************************************** */
+/* ***********************************_ИЛИ_********************************** */
+/* ************************************************************************** */
+
+
+#include "ft_stock_par.h"
+
 int					ft_strlen(char *str)
 {
 	int i;
@@ -48,3 +97,22 @@ struct s_stock_par	*ft_param_to_tab(int ac, char **av)
 	ret[i].str = 0;
 	return (ret);
 }
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int		main(int argc, char **argv)
+{
+	struct s_stock_par *a;
+	a = ft_param_to_tab(argc, argv);
+	ft_show_tab(a);
+	return (0);
+}
+
+
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+/* ************************************************************************** */
