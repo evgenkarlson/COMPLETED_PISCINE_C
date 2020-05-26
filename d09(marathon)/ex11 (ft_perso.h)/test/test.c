b5bf444414e24d69e67626ed1108c99033371585
evgenkarlson <V1z9l7o3m5@mail.ru>
2020-05-26 22:39:06 +0300

    26.05.2020

---
+++ d09(marathon)/ex11 (ft_perso.h)/test/test.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: evgenkarlson <RTFM@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/12 12:33:14 by evgenkarlson      #+#    #+#             */
+/*   Updated: 2020/05/12 10:51:23 by evgenkarlson     ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+/*	команда для компиляции и одновременного запуска                           */
+/*                                                                            */
+/*  gcc -Wall -Werror -Wextra test.c && chmod +x ./a.out && ./a.out	   	      */
+/* ************************************************************************** */
+
+
+#include <unistd.h>
+#include "ft_perso.h"
+
+void			ft_putchar(char c)		/* функция печати символа */
+{
+	write(1, &c, 1);
+}
+
+void			ft_putstr(char *str)	/* функция печати строки */
+{
+	while(*str)
+		ft_putchar(*str++);
+}
+
+void			ft_putnbr(int nb)		/* Функция печати числа */
+{
+	int	temp;
+	int	size;
+
+	size = 1;
+
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;					
+	}
+	if (nb == -2147483648)
+	{	
+		ft_putchar('2');
+		nb = 147483648;
+	}
+	temp = nb;
+
+	while ((temp /= 10) > 0)
+		size *= 10;
+	temp = nb;
+	while (size)
+	{
+		ft_putchar((char)((temp / size)) + 48);
+		temp %= size;
+		size /= 10;
+	}
+}
+
+int				main()
+{
+	t_perso		donnie;
+	donnie.name = strdup("Donnie");
+	donnie.life = 100.0;
+	donnie.age = 24;
+	donnie.profession = SAVE_AUSTIN_POWERS;
+	(void)donnie;
+	
+	ft_putstr(donnie.name);
+	ft_putchar('\n');
+	ft_putnbr(donnie.age);
+	ft_putchar('\n');
+	ft_putstr(donnie.profession);
+	ft_putchar('\n');
+	
+	return (0);
+}
