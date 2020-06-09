fec2195723be3a633ba920b85dd2b113dfe0a23f
evgenkarlson <V1z9l7o3m5@mail.ru>
2020-06-09 20:28:59 +0300

    09.06.2020

--- d09(marathon)/ex14 (ft_decrypt)/ft_decrypt.c
+++ d09(marathon)/ex14 (ft_decrypt)/ft_decrypt.c
@@ -88,26 +88,116 @@ t_perso		**ft_decrypt(char *str)
 		while (str && str[j++] == '|')
 			numstructs++;
 	}
-	j = 0;
-	if ((tab = (t_perso **)malloc(sizeof(t_perso *) * (numstructs + 1))) == ((void *)0))
-		return ((void *)0);
-	while (j < numstructs)
+	if (numstructs > 0)
 	{
-		if ((*(tab + j) = (t_perso *)malloc(sizeof(t_perso))) == ((void *)0))
+		j = 0;
+		if ((tab = (t_perso **)malloc(sizeof(t_perso *) * (numstructs + 1))) == ((void *)0))
 			return ((void *)0);
-		while(*str && (ft_isspace(*str) || !(*str >= '0' && *str <= '9')))
-			str++;
-		(*(tab + j))->age = ft_get_age(str);
-		while(*str && (ft_isspace(*str) || (*str >= '0' && *str <= '9')))
-			str++;
+		while (j < numstructs)
+		{
+			if ((*(tab + j) = (t_perso *)malloc(sizeof(t_perso))) == ((void *)0))
+				return ((void *)0);
+			while(*str && (ft_isspace(*str) || !(*str >= '0' && *str <= '9')))
+				str++;
+			(*(tab + j))->age = ft_get_age(str);
+			while(*str && (ft_isspace(*str) || (*str >= '0' && *str <= '9')))
+				str++;
 
-		(*(tab + j))->name = ft_get_name(str);
-		j++;
-		str++;
+			(*(tab + j))->name = ft_get_name(str);
+			j++;
+			str++;
+		}
+		return (tab);
+	}
+	return ((void *)0);
+}
+
+
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+
+
+
+#include <stdlib.h>
+#include "ft_perso.h"
+
+int			ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char		*ft_get_name(int *i, char *str)
+{
+	int		j;
+	char	*name;
+
+	j = 0;
+	name = (char *)malloc(sizeof(char) * ft_strlen(str) + 1);
+	while ((str[*i] >= 'a' && str[*i] <= 'z')
+			|| (str[*i] >= 'A' && str[*i] <= 'Z'))
+		name[j++] = str[*i++];
+	name[j] = '\0';
+	return (name);
+}
+
+int			ft_get_age(int *i, char *str)
+{
+	int age;
+
+	age = 0;
+	while (str[*i] >= '0' && str[*i] <= '9')
+		age = (age * 10) + (str[*i++] - '0');
+	return (age);
+}
+
+t_perso		**ft_create_struct_tab(char *str, int nbstructs)
+{
+	int		i;
+	int		j;
+	t_perso	**tab;
+
+	i = 0;
+	j = 0;
+	tab = (t_perso **)malloc(sizeof(t_perso**) * (nbstructs + 1));
+	while (str[i])
+	{
+		tab[j] = (t_perso *)malloc(sizeof(t_perso));
+		tab[j]->age = ft_get_age(&i, str);
+		i += 1;
+		tab[j]->name = ft_get_name(&i, str);
+		i += 1;
+		j += 1;
 	}
 	return (tab);
 }
 
-/* ************************************************************************** */
-/* ************************************************************************** */
-/* ************************************************************************** */
+t_perso		**ft_decrypt(char *str)
+{
+	int i;
+	int nbstructs;
+
+	i = 0;
+	nbstructs = 0;
+	while (str[i])
+	{
+		while (str[i] >= '0' && str[i] <= '9')
+			i += 1;
+		if (str[i] == '|')
+			i += 1;
+		while ((str[i] >= 'a' && str[i] <= 'z')
+				|| (str[i] >= 'A' && str[i] <= 'Z'))
+			i += 1;
+		if (str[i] == ';')
+			i += 1;
+		nbstructs += 1;
+	}
+	return (ft_create_struct_tab(str, nbstructs));
+}
