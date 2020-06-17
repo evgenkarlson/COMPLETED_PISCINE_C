5d3a7948acbda54981be0850f195b095b90e4db7
evgenkarlson <V1z9l7o3m5@mail.ru>
2020-06-17 19:27:18 +0300

    17.06.2020

--- test.c
+++ test.c
@@ -11,68 +11,58 @@
 /* ************************************************************************** */
 /*	команда для компиляции и одновременного запуска                           */
 /*                                                                            */
-/* gcc -Wall -Werror -Wextra main.c ft_fight.c && chmod +x ./a.out && ./a.out */
+/*  gcc -Wall -Werror -Wextra test.c && chmod +x ./a.out && ./a.out	   	      */
 /* ************************************************************************** */
 
-#include "ft_ultimate_fight.h"
+#include <unistd.h>				/* Подключаем библиотеку содержащую функцию "write" */
+#include <stdlib.h>				/* Подключаем библиотеку содержащую функцию "malloc" */
 
-void	ft_putstr(char *str)
+/* ************************************************************************** */
+void		ft_putchar(char c)	/* Функция печати символа */
 {
-	while (*str)
-		write(1, str++, 1);
+	write(1, &c, 1);
 }
-
-void	ft_fight(t_perso *attacker, t_perso *defense, int n)
+/* ************************************************************************** */
+void		ft_putstr(char *str)/* Функция печати строки */
 {
-	const char	*attack[14] = { "punch", "kick", "chop ichi", "chop ni",
-		"chop san", "chop shi", "chop go", "chop roku", "chop shichi",
-		"chop hachi", "chop ku", "chop ju", "chop hyaku", "headbutt" };
-	const int	points[14] = {15, 5, 13, 3, 18, 9, 11, 13, 8, 6, 11, 9, 18, 20};
+	while(*str)
+		ft_putchar(*str++);
+}
+/* ************************************************************************** */
+void		ft_putnbr(int nb)	/* Функция печати числа */
+{
+	int	temp;
+	int	size;
 
-	if (n > 14)
-		n %= 14;
-	if (attacker->life <= 0 || defense->life <= 0)
-		return ;
-	defense->life -= points[n];
-	ft_putstr(attacker->name);
-	ft_putstr(" does a judo ");
-	ft_putstr((char *)attack[n]);
-	ft_putstr(" on ");
-	ft_putstr(defense->name);
-	ft_putstr("\n");
-	if (defense->life <= 0)
+	size = 1;
+	if (nb < 0)
 	{
-		ft_putstr(defense->name);
-		ft_putstr(" is dead.\n");
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb == -2147483648)
+	{	
+		ft_putchar('2');
+		nb = 147483648;
+	}
+	temp = nb;
+	while ((temp /= 10) > 0)
+		size *= 10;
+	temp = nb;
+	while (size)
+	{
+		ft_putchar((char)((temp / size)) + 48);
+		temp %= size;
+		size /= 10;
 	}
 }
+/* ************************************************************************** */
 
-int		main(void)
+
+
+int 		main(void)
 {
-	t_perso		donnie_matrix;			/* Создаем экземпляр структуры (персонаж "Donnie Matrix") */
-	t_perso		frau_farbissina;		/* Создаем экземпляр структуры (персонаж "Frau Farbissina") */
-
-	donnie_matrix = (t_perso) {.name = "Donnie Matrix", .life = 100.0};		/* Инициализируем структуру заполняя ее данными */
-	frau_farbissina = (t_perso) {.name = "Frau Farbissina", .life = 100.0};	/* Инициализируем структуру заполняя ее данными */
-
-	/* Далее вызываем функцию "ft_fight" столько раз сколько нам нужно и отправляем ей в аргументы экземпляры и макросы немного 
-	 * изменяя их чтобы создать иллюзию схватки */
-
-	ft_fight(&donnie_matrix, &frau_farbissina, KICK);
-	ft_fight(&frau_farbissina, &donnie_matrix, PUNCH);
-	ft_fight(&donnie_matrix, &frau_farbissina, KICK1);
-	ft_fight(&frau_farbissina, &donnie_matrix, KICK2);
-	ft_fight(&donnie_matrix, &frau_farbissina, KICK3);
-	ft_fight(&frau_farbissina, &donnie_matrix, KICK4);
-	ft_fight(&donnie_matrix, &frau_farbissina, KICK5);
-	ft_fight(&frau_farbissina, &donnie_matrix, KICK6);
-	ft_fight(&donnie_matrix, &frau_farbissina, KICK7);
-	ft_fight(&frau_farbissina, &donnie_matrix, KICK8);
-	ft_fight(&donnie_matrix, &frau_farbissina, KICK9);
-	ft_fight(&frau_farbissina, &donnie_matrix, KICK10);
-	ft_fight(&donnie_matrix, &frau_farbissina, KICK11);
-	ft_fight(&donnie_matrix, &frau_farbissina, HEADBUTT);
-
-
-    return (0);							/* Возвращаем ноль и завершаем функцию */
+	
+	ft_putchar('\n');
+    return (0);						/* Возвращаем 0 и завершаем программу */
 }
