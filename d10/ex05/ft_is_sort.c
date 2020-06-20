7b725377e4ae133a880830d912fcaf9d66b4ff3a
evgenkarlson <V1z9l7o3m5@mail.ru>
2020-06-20 21:22:37 +0300

    20.06.2020

--- d10/ex05/ft_is_sort.c
+++ d10/ex05/ft_is_sort.c
@@ -14,24 +14,24 @@ int	ft_is_sort(int *tab, int length, int(*f)(int, int))
 	int	i;
 
 	i = 0;
-	while (i < length - 1)
+	while (i < (length - 1))
 	{
 		if (f(tab[i], tab[i + 1]) >= 0)
 			i++;
 		else
 			i = length;
 	}
-	if (i == length - 1)
+	if (i == (length - 1))
 		return (1);
 	i = 0;
-	while (i < length - 1)
+	while (i < (length - 1))
 	{
 		if (f(tab[i], tab[i + 1]) <= 0)
 			i++;
 		else
 			i = length;
 	}
-	if (i == length - 1)
+	if (i == (length - 1))
 		return (1);
 	return (0);
 }
