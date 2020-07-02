740244e6e3a0c7c35cad3e378fa4b2e9fdce2946
evgenkarlson <V1z9l7o3m5@mail.ru>
2020-07-02 16:32:50 +0300

    02.06.2020

--- d08/ex04(ft_point)/test/test.c
+++ d08/ex04(ft_point)/test/test.c
@@ -19,11 +19,38 @@
 #include "unistd.h"
 
 
-void	ft_putchar(char c)
+void	ft_putchar(char c)		/* Функция печати символа */
 {
 	write(1, &c ,1);
 }
 
+void	ft_putnbr(int nb)		/* Функция печати числа */
+{
+	int	temp;
+	int	size;
+
+	size = 1;
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb == -2147483648)
+	{	
+		ft_putchar('2');
+		nb = 147483648;
+	}
+	temp = nb;
+	while ((temp /= 10) > 0)
+		size *= 10;
+	temp = nb;
+	while (size)
+	{
+		ft_putchar((char)((temp / size)) + 48);
+		temp %= size;
+		size /= 10;
+	}
+}
 
 void	set_point(t_point *point)
 {
@@ -38,12 +65,17 @@ int		main(void)
 
 	set_point(&point);
 
+	ft_putnbr(point.x);
+	ft_putchar(' ');
+	ft_putnbr(point.y);
+
+/*	ИЛИ так
 	ft_putchar(point.x/10 + '0');
 	ft_putchar(point.x%10 + '0');
 	ft_putchar(' ');
 	ft_putchar(point.y/10 + '0');
 	ft_putchar(point.y%10 + '0');
-
+*/
 	return (0);
 }
 
