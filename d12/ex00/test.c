3ee2f835c51dab795987190c455c83c8f74e2c4f
evgenkarlson <V1z9l7o3m5@mail.ru>
2020-07-27 12:50:49 +0300

    12:50/27/07/2020

--- d12/ex00/test.c
+++ d12/ex00/test.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_display_file.c                                  :+:      :+:    :+:   */
+/*   test.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: evgenkarlson <RTFM@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -10,12 +10,48 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #include <unistd.h>
 #include <fcntl.h>
-#include "ft_lib.h"
 
 #define NONE	{ft_putstr("No such file or directory.\n"); return ;}
 #define DIR		{ft_putstr(argv[1]);ft_putstr(": Is a directory.\n");return;}
+#define BUFF_SIZE	4096
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (*(str + i))
+		i++;
+	write(1, str, i);
+}
+
+int		ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
+
+void	ft_display_stdin(int file)
+{
+	char	buffer[BUFF_SIZE + 1];
+
+	while (read(file, buffer, BUFF_SIZE))
+		ft_putstr(buffer);
+}
+
 
 void	ft_display_file(int argc, char **argv)
 {
@@ -28,6 +64,7 @@ void	ft_display_file(int argc, char **argv)
 			ft_strcmp(argv[1], "../") == 0)
 			DIR;
 		file = open(argv[1], O_RDONLY);
+		
 		if (file == -1)
 			NONE;
 		ft_display_stdin(file);
@@ -37,4 +74,10 @@ void	ft_display_file(int argc, char **argv)
 		ft_putstr("Too many arguments.\n");
 	if (argc < 2)
 		ft_putstr("File name missing.\n");
+}
+
+int		main(int argc, char **argv)
+{
+	ft_display_file(argc, argv);
+	return (0);
 }
