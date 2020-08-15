05eebcc07af356d6ca2f179ce241dd86d99c535f
evgenkarlson <V1z9l7o3m5@mail.ru>
2020-08-15 12:45:05 +0300

    12:45/15/08/2020

--- d02/ex07(ft_print_combn)/ft_print_combn.c
+++ d02/ex07(ft_print_combn)/ft_print_combn.c
@@ -24,54 +24,6 @@ void	ft_putchar(char c);
 
 /* ************************************************************************** */
 
-static void	ft_me_cago_en_norminette(int *arr, int col, int n)
-{
-	int		i;
-
-	while (col)
-	{
-		while (arr[col] == (9 + col - (n - 1)) && col >= 0)
-			col -= 1;
-		if (col < 0)
-			break ;
-		arr[col] += 1;
-		i = (col + 1) - 1;
-		while (i++ < n)
-			arr[i] = arr[i - 1] + 1;
-		col = n - 1;
-		ft_putchar(',');
-		ft_putchar(' ');
-		i = 0;
-		while (i < n)
-			ft_putchar(arr[i++] + '0');
-	}
-}
-
-void		ft_print_combn(int n)
-{
-	int		arr[n];
-	int		i;
-	int		col;
-
-	col = n - 1;
-	if (n > 10)
-		return ;
-	i = -1;
-	while (i++ < n)
-		arr[i] = i;
-	i = 0;
-	while (i < n)
-		ft_putchar(arr[i++] + '0');
-	ft_me_cago_en_norminette(arr, col, n);
-	ft_putchar('\n');
-}
-
-
-/* ************************************************************************** */
-/* *********************************_ИЛИ_************************************ */
-/* ************************************************************************** */
-
-
 void	ft_check_print(int x[], int n)
 {
 	int i;
@@ -132,5 +84,57 @@ void	ft_print_combn(int n)
 	create_while(x, 0, n);
 }
 
+/* ************************************************************************** */
+/* *********************************_ИЛИ_************************************ */
+/* ************************************************************************** */
+
+
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+/* ************************************************************************** */
+
+static void	ft_me_cago_en_norminette(int *arr, int col, int n)
+{
+	int		i;
+
+	while (col)
+	{
+		while (arr[col] == (9 + col - (n - 1)) && col >= 0)
+			col -= 1;
+		if (col < 0)
+			break ;
+		arr[col] += 1;
+		i = (col + 1) - 1;
+		while (i++ < n)
+			arr[i] = arr[i - 1] + 1;
+		col = n - 1;
+		ft_putchar(',');
+		ft_putchar(' ');
+		i = 0;
+		while (i < n)
+			ft_putchar(arr[i++] + '0');
+	}
+}
+
+void		ft_print_combn(int n)
+{
+	int		arr[n];
+	int		i;
+	int		col;
+
+	col = n - 1;
+	if (n > 10)
+		return ;
+	i = -1;
+	while (i++ < n)
+		arr[i] = i;
+	i = 0;
+	while (i < n)
+		ft_putchar(arr[i++] + '0');
+	ft_me_cago_en_norminette(arr, col, n);
+	ft_putchar('\n');
+}
 
 /* ************************************************************************** */
