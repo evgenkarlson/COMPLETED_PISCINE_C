51203b5ea9a90a4a7733ac484a70c9dc2c19309c
evgenkarlson <V1z9l7o3m5@mail.ru>
2020-11-17 02:12:39 +0300

    c12 is completed

--- c12/ex12(ft_list_remove_if)/ft_list_remove_if.c
+++ c12/ex12(ft_list_remove_if)/ft_list_remove_if.c
@@ -6,38 +6,57 @@
 /*   By: evgenkarlson <RTFM@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/14 12:33:14 by evgenkarlson      #+#    #+#             */
-/*   Updated: 2020/02/15 10:51:23 by evgenkarlson     ###   ########.fr       */
+/*   Updated: 2020/11/17 01:16:53 by evgenkarlson     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 /* ************************************************************************** */
-/* ************************************************************************** *
+/* ************************************************************************** **
+**
+**  Каталог сдачи задания: ex12/
+**  Файлы для сдачи: ft_list_remove_if.c, ft_list.h
+**  Разрешенные функции: Никаких
+**
+** ************************************************************************** **
  *
  * 
- *	• Создайте функцию ft_list_remove_if, которая удаляет из списка все элемен-
- *	ты, данные которых «равны» справочным данным.
+ *	• Создайте функцию ft_list_remove_if, которая удаляет из списка связанных
+ *  между собой структур все элементы, у которых данные в 'data' будут «равны» 
+ *  справочным данным в 'data_ref'. 
+ *  Сравнение производиться с помощью 'cmp', если данные «равны» то 'cmp' должна
+ *  вернуть 0.
+ *
+ * 
+ *	• Данные из элемента, который нужно стереть, нужно освободить с помощью
+ *  'free_fct'.
  * 
  * 
- *	• Вот как эта функция должна быть объявлена :
+ *	• Вот как эта функция должна быть объявлена:
  *
  *	void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)());
  * 
+ * 
+ *	• Функции, обозначенные как 'cmp' и 'free_fct', будут использоваться
+ *  следующим образом:
+ * 
+ *											(*cmp)(list_ptr->data, data_ref);
+ * 											(*free_fct)(list_ptr->data);
+ *
  *
  * ************************************************************************** */
 /* ************************************************************************** */
-/* ************************************************************************** */
+
 
 #include <stdlib.h>
 #include "ft_list.h"
 
-void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(), void (*free_fct)(void *))
 {
 	t_list	*last;
 	t_list	*current;
-	t_list	*tmp;
 
 	last = ((void *)0);
 	current = *begin_list;
-	tmp = ((void *)0);
 	while (current)
 	{
 		if ((*cmp)(current->data, data_ref) == 0)
@@ -46,9 +65,10 @@ void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 				*begin_list = current->next;
 			else
 				last->next = current->next;
-			tmp = current;
+			last = current;
 			current = current->next;
-			free(tmp);
+			free_fct(last->data);
+			free(last);
 		}
 		else
 		{
@@ -61,33 +81,34 @@ void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 
 /* ************************************************************************** */
 /* ************************************************************************** */
-/* ************************************************************************** */
+
 
 #include <stdlib.h>
 #include "ft_list.h"
 
-void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(), void (*free_fct)(void *))
 {
-	t_list *list_ptr;
-	t_list *list_parent;
+	t_list *current;
+	t_list *last;
 	t_list *tmp;
 
-	list_ptr = *begin_list;
-	list_parent = ((void *)0);
-	while (list_ptr)
+	current = *begin_list;
+	last = ((void *)0);
+	while (current)
 	{
-		tmp = list_ptr;
-		if ((*cmp)(list_ptr->data, data_ref) == 0)
+		tmp = current;
+		if ((*cmp)(current->data, data_ref) == 0)
 		{
-			if (list_parent != 0)
-				list_parent->next = list_ptr->next;
-			free(list_ptr);
+			if (last)
+				last->next = current->next;
+			free_fct(current->data);
+			free(current);
 		}
-		list_parent = list_ptr;
-		list_ptr = tmp->next;
+		last = current;
+		current = tmp->next;
 	}
 }
 
-/* ************************************************************************** */
+
 /* ************************************************************************** */
 /* ************************************************************************** */
