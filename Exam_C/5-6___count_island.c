e93fb69cfdc4ac495913030409c9ad4b88f9f9d0
evgenkarlson <V1z9l7o3m5@mail.ru>
2021-02-23 01:29:50 +0300

    update

--- Exam_C/5-6___count_island.c
+++ Exam_C/5-6___count_island.c
@@ -6,7 +6,7 @@
 /*   By: evgenkarlson <RTFM@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/14 12:33:14 by evgenkarlson      #+#    #+#             */
-/*   Updated: 2021/02/23 00:47:00 by evgenkarlson     ###   ########.fr       */
+/*   Updated: 2021/02/23 01:24:21 by evgenkarlson     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -118,3 +118,303 @@ $>
 
 ** ************************************************************************** */
 /* ************************************************************************** */
+
+
+#include <unistd.h>
+#include <fcntl.h>
+
+void	flood_fill(char *map, int i, char c, int width, int total_b)
+{
+	map[i] = c;
+	if (i > width && map[i - width] == 'X')
+		flood_fill(map, i - width, c, width, total_b);
+	if (i + width < total_b - 1 && map[i + width] == 'X')
+		flood_fill(map, i + width, c, width, total_b);
+	if (i - 1 >= 0 && map[i - 1] == 'X')
+		flood_fill(map, i - 1, c, width, total_b);
+	if (i + 1 < total_b - 1 && map[i + 1] == 'X')
+		flood_fill(map, i + 1, c, width, total_b);
+}
+
+int	main(int ac, char **av)
+{
+	if (ac != 2)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+	char map[102400] = {0};
+	int fd = open(av[1], O_RDONLY);
+	int total_b = read(fd, map, 102400);
+
+	if (total_b <= 0)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+	int width = 1;
+	int i = 0;
+	while (map[i] != '\n')
+	{
+	 	i++;
+		width++;
+	}
+	int height = 0;
+	for (int i = 0; i < total_b; i++)
+	{
+		if (map[i] != 'X' && map[i] != '.' && map[i] != '\n' && map[i] != '\0')
+		{
+			write(1, "\n", 1);
+        	return (0);
+		}
+		if (map[i] == '\n')
+			height++;
+	}
+
+	if (height * width != total_b)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+	char replacer = '0';
+	for (int i = 0; i < total_b; i++)
+	{
+		
+		if (map[i] == 'X') 
+		{
+			flood_fill(map, i, replacer, width, total_b);
+			replacer++;
+		}
+	}
+	if (replacer > ':')
+	{
+		write(1, "\n", 1);
+        return (0);
+	}
+	write(1, map, total_b);
+}
+
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+void	count_island(char **i, int x, int y, char c);
+void	sefsrg(char **i);
+char	**cread(int fd);
+
+void		count_island(char **i, int x, int y, char c)
+{
+	if (y < 0 || x < 0 || !i[y] || !i[y][x] || i[y][x] != 'X')
+		return ;
+	i[y][x] = c;
+	count_island(i, x + 1, y, c);
+	count_island(i, x - 1, y, c);
+	count_island(i, x, y + 1, c);
+	count_island(i, x, y - 1, c);
+}
+
+void		sefsrg(char **i)
+{
+	int		x;
+	int		y;
+	char	c;
+
+	y = 0;
+	c = '0';
+	while (i[y])
+	{
+		x = 0;
+		while (i[y][x])
+		{
+			if (i[y][x] == 'X')
+				count_island(i, x, y, c++);
+			write(1, i[y] + x, 1);
+			x++;
+		}
+		y++;
+		if (x > 0)
+			write(1, "\n", 1);
+	}
+}
+
+int			read_first_line(int fd, char **r)
+{
+	int		i = 0;
+
+	r[0] = malloc(sizeof(char) * 1300);
+	while (read(fd, r[0] + i, 1) > 0)
+	{
+		if (r[0][i] == '\n')
+		{
+			r[0][i] = 0;
+			return (i);
+		}
+		else if (r[0][i] != '.' && r[0][i] != 'X')
+			return (-1);
+		i++;
+	}
+	return (-1);
+}
+
+int			read_one_line(int fd, int len, char **r)
+{
+	int		readed;
+
+	*r = malloc(sizeof(char) * (len + 10));
+	readed = read(fd, *r, len + 1);
+	if (readed == -1)
+		return (-1);
+	if (readed == 0)
+		return (0);
+	if (readed == 1 && **r == '\n')
+	{
+		**r = 0;
+		return (0);
+	}
+	if (readed < len)
+		return (-1);
+	if (r[0][len] != '\n')
+		return (-1);
+	r[0][len] = 0;
+	readed -= 2;
+	while (readed >= 0)
+	{
+		if (r[0][readed] != 'X' && r[0][readed] != '.')
+			return (-1);
+		readed--;
+	}
+	return (1);
+}
+
+char		**cread(int fd)
+{
+	char	**result;
+	int		i;
+	int		linelen;
+	int		ret;
+
+	result = malloc(sizeof(char *) * 100000);
+	i = 0;
+	linelen = read_first_line(fd, result);
+	if (linelen < 1)
+		return (0);
+	i++;
+	while (1)
+	{
+		ret = read_one_line(fd, linelen, result + i);
+		if (ret == -1)
+			return (0);
+		if (ret == 0)
+			break ;
+		i++;
+	}
+	return (result);
+}
+
+int		main(int ac, char **av)
+{
+	int		fd;
+	char	**a;
+
+	if (ac > 1)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd != -1)
+		{
+			a = cread(fd);
+			close(fd);
+			if (a != 0)
+			{
+				sefsrg(a);
+				return (0);
+			}
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
+
+
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define LIM 100000
+
+int errorCheck(char buf[LIM], int size, int width, int height) {
+
+	if (size / height != width) {
+		write(1, "\n", 1);
+		return 0;
+	}
+
+	for (int i = 0; i < size; i++) {
+		if (buf[i] != '.' && buf[i] != 'X' && buf[i] != '\n')
+			return 0;
+	}
+	
+	return 1;
+}
+
+void floodFill(char buf[LIM], int size, int width, int height, int i, char replacer) {
+
+	buf[i] = replacer;
+
+	if ((i > width) && (buf[i - width] == 'X'))
+		floodFill(buf, size, width, height, i - width, replacer);
+
+	if ((i < size - width - 1) && buf[i + width] == 'X')
+		floodFill(buf, size, width, height, i + width, replacer);
+
+	if (i && (i % width > 0) && buf[i - 1] == 'X')
+		floodFill(buf, size, width, height, i - 1, replacer);
+
+	if (i && (i % width < width - 1) && buf[i + 1] == 'X')
+		floodFill(buf, size, width, height, i + 1, replacer);
+}
+
+void countIsland(char* file) {
+	
+	char buf[LIM] = {0};
+	int fd = open(file, O_RDONLY);
+
+	int size = read(fd, buf, LIM);
+	if (size < 0) {
+		write(1, "\n", 1);
+		return;
+	}
+
+	int width = 1;
+	for (int i = 0; buf[i] != '\n'; i++)
+		width++;
+
+	int height = size / width;
+
+	if (!errorCheck(buf, size, width, height))
+		return;
+
+	char replacer = '0';
+	for (int i = 0; i < size; i++) {
+		if (buf[i] == 'X') {
+			floodFill(buf, size, width, height, i, replacer);
+			replacer++;
+		}
+	}
+	write(1, buf, size);
+}
+
+int main(int ac, char* av[]) {
+	if (ac == 2)
+		countIsland(av[1]);
+	return 0;
+}
