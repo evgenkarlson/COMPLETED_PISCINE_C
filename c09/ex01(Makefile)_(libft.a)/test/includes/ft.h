4a6b340c370f04f508dd7984603dc52cbd67773a
evgenkarlson <V1z9l7o3m5@mail.ru>
2021-04-14 19:00:31 +0300

    19:00/14/04/2021

--- c09/ex01(Makefile)_(libft.a)/test/includes/ft.h
+++ c09/ex01(Makefile)_(libft.a)/test/includes/ft.h
@@ -6,19 +6,18 @@
 /*   By: evgenkarlson <RTFM@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/12 12:33:14 by evgenkarlson      #+#    #+#             */
-/*   Updated: 2021/04/14 15:01:24 by evgenkarlson     ###   ########.fr       */
+/*   Updated: 2021/04/14 15:11:13 by evgenkarlson     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_H
 # define FT_H
+# include <unistd.h>
 
 void	ft_putchar(char c);
 void	ft_putstr(char *str);
 int		ft_strcmp(char *s1, char *s2);
 int		ft_strlen(char *str);
 void	ft_swap(int *a, int *b);
-
-# include <unistd.h>
 
 #endif
