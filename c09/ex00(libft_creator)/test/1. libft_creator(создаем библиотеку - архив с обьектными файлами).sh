db253e7aefb91f1a4d78cc40c51726ac491fa479
evgenkarlson <V1z9l7o3m5@mail.ru>
2021-04-14 14:57:48 +0300

    14:57/14/04/2021

--- c09/ex00(libft_creator)/test/1. libft_creator(создаем библиотеку - архив с обьектными файлами).sh
+++ c09/ex00(libft_creator)/test/1. libft_creator(создаем библиотеку - архив с обьектными файлами).sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-gcc -Wall -Wextra -Werror -c ft_putchar.c ft_swap.c ft_putstr.c ft_strlen.c ft_strcmp.c
+
+################################################################################
+#
+#           1. libft_creator(создаем библиотеку - архив с обьектными файлами) :
+#
+################################################################################
+
+gcc -Wall -Wextra -Werror -c ft_putchar.c ft_swap.c ft_putstr.c ft_strlen.c ft_strcmp.c -I./
+#  ИЛИ ТАК
+# 
+# for f in *.c; do gcc -Wall -Wextra -Werror -c $f -I./; done
+
 ar rc libft.a ft_putchar.o ft_swap.o ft_putstr.o ft_strlen.o ft_strcmp.o
+
 ranlib libft.a
+
 rm *.o
